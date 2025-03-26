42e7bffca21c51df433de470cd44ca66a1cc8ca5
Gabriel Zapata <zapata.axuariogabriel@gmail.com>
2025-03-25 23:33:10 -0500

    Primer commit

---
+++ src/main/java/com/example/SmartPayroll/SmartPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartPayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPayrollApplication.class, args);
+	}
+
+}
